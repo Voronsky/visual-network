4633938681b4497a8f6dd6f5fb87ec8aa49487c7
Ivan Diaz <roflocopter911@gmail.com>
2018-05-15 21:19:40 +0000

    Added server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 var express = require('express');
 var app = express();
+
+app.use('/node_modules',express.static(__dirname + '/node_modules'));
+app.use('/js',express.static(__dirname+'/js'));
+app.get('/', (req,res)=>{
+
+    // Load the network map
+    res.sendFile('./index.html', {root: __dirname});
+
+    if(req.method === 'POST'){
+
+    }
+});
+
+app.listen(8080);

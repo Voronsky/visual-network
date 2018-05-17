948c11db50063a630db1c8bba8e9c802124a0045
Ivan Diaz <roflocopter911@gmail.com>
2018-05-17 21:01:35 +0000

    Added checking for 3 different edge cases of node creation

--- js/main.js
+++ js/main.js
@@ -20,10 +20,12 @@ function setup(){
 }
 
 function updateMap() {
-    let dst = { id: ++counter, label: document.getElementById("dst").value};
-    console.log(dst);
     let src = {id: ++counter, label:document.getElementById("src").value};
     console.log(src);
+    let dst = { id: ++counter, label: document.getElementById("dst").value};
+    console.log(dst);
+
+    //Check to see if any may exist already that have the exact labels
     var ids = {
         src: nodes.getIds({
             filter: function(node){
@@ -36,10 +38,20 @@ function updateMap() {
             }
         })
     };
-    console.log(id);
-    if(id[0] > 0 ||  id.length > 0){
+    console.log(ids.src);
+    let sid = ids.src;
+    console.log(ids.dst);
+    let did = ids.dst;
+
+    //If the SRC id already exists but the DST does not
+    //else if the SRC and DST ids already exist, update the edge
+    //else add 2 new nodes
+    if(sid.length != 0 && did.length === 0){
         nodes.update([{id:dst.id, label: dst.label}]);
-        edges.update({from: id[0], to: dst.id});
+        edges.update({from: sid[0], to: dst.id});
+    }
+    else if(did.length != 0 && sid.length != 0){
+        edges.update({from: sid[0], to: did[0]});
     }
     else{
         nodes.update([{id: src.id, label: src.label},
